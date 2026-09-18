097fec683250ac5fa1785e87cc9c8f32e0beccc4
Mohd Faizan <mohammadfaizan7844@gmail.com>
2026-09-18 16:43:57 +0530

    Update database configuration

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_NAME = "cafe_qr_db"
+import os
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "cafe_qr_db")
